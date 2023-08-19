2084338a06b369c255106f70e1b29796cf3c08ff
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-08-19 11:07:30 +0900

    【1-3-4-1】gem『letter_opener_web』の導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,10 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = true
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
