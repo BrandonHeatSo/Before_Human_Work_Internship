77971d2423f572fd3b09ef60e65e2b4d30758ceb
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2023-08-20 10:34:47 +0900

    【1-3-5】letter_opener_web設定完了【1-3-6】edit画面にnameフォーム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.perform_caching = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_caching = true
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
